7330a0060745c14c3e030b62814b114dd64972cd
Olly Welch <mail@ollywelch.com>
2024-12-03 09:41:18 +0000

    Day 3: Complete

--- day3/day3_test.go
+++ day3/day3_test.go
@@ -38,3 +38,35 @@ func TestParse(t *testing.T) {
 		})
 	}
 }
+
+func TestParse2(t *testing.T) {
+	type args struct {
+		input string
+	}
+	tests := []struct {
+		name  string
+		args  args
+		want  []int
+		want1 []int
+	}{
+		{
+			name: "Should parse the input correctly",
+			args: args{
+				input: `xmul(2,4)&mul[3,7]!^don't()_mul(5,5)+mul(32,64](mul(11,8)undo()?mul(8,5))`,
+			},
+			want:  []int{2, 8},
+			want1: []int{4, 5},
+		},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			got, got1 := day3.Parse2(tt.args.input)
+			if !reflect.DeepEqual(got, tt.want) {
+				t.Errorf("Parse2() got = %v, want %v", got, tt.want)
+			}
+			if !reflect.DeepEqual(got1, tt.want1) {
+				t.Errorf("Parse2() got1 = %v, want %v", got1, tt.want1)
+			}
+		})
+	}
+}
--- solutions/day3/main.go
+++ solutions/day3/main.go
@@ -14,12 +14,21 @@ func main() {
 		log.Fatal(err)
 	}
 	nums1, nums2 := day3.Parse(string(inputBytes))
+	nums3, nums4 := day3.Parse2(string(inputBytes))
 	total := 0
+	total2 := 0
 	if len(nums1) != len(nums2) {
 		log.Fatal(fmt.Errorf("nums1 length did not match nums2: %d != %d", len(nums1), len(nums2)))
+	}
+	if len(nums3) != len(nums4) {
+		log.Fatal(fmt.Errorf("nums3 length did not match nums4: %d != %d", len(nums3), len(nums4)))
 	}
 	for i := range nums1 {
 		total += nums1[i] * nums2[i]
 	}
+	for i := range nums3 {
+		total2 += nums3[i] * nums4[i]
+	}
 	fmt.Println("Total:", total)
+	fmt.Println("Total 2:", total2)
 }
